08977fa486b6b9464776a5985529ceccb47144f8
Rushik kumar Avula <rushik-010801>
2021-05-13 18:24:48 +0530

    small logout change

--- admin_logout.php
+++ admin_logout.php
@@ -2,7 +2,7 @@
 	session_start();
 	if(isset($_SESSION['name'])){
 		session_destroy();
-		echo "<script> location.href='Admin_login.html'</script>";
+		echo "<script> location.href='index.html'</script>";
 	}
 
 ?>
