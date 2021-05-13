9407d0bc8b62de1d8057b9be1a83cb714ba4fc6d
amit-tech01 <amit.kumar@ecerasystem.com>
2021-05-13 12:48:14 +0530

    UI with CRUD and search feature

--- admin_login.php
+++ admin_login.php
@@ -1,10 +1,18 @@
 <?php 
   $uname=$_POST["tf1"]; 
   $pass=$_POST["tf2"]; 
-  if($uname=="administrator" AND $pass=="admin123"){
+  if($uname=="admin" AND $pass=="admin123"){
       session_start();
       $_SESSION['name']=$uname;
     echo "<script>location.href='admin_home.php'</script>";
+  }
+  else if($uname=="amit" AND $pass=="ammy"){
+    session_start();
+    $_SESSION['name']=$uname;
+  echo "<script>location.href='admin_home.php'</script>";
+  }
+  else if($uname=="" AND $pass==""){
+    echo "<script>alert('Welcome!!');</script>";
   }
   else{
     echo "<script>alert('Incorrect credentials');</script>";
