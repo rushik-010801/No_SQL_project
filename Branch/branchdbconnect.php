935cfce9c70290f9a6406941ae3a12ba24ea0e57
Amit Kumar Sahu <62533482+ammy20019@users.noreply.github.com>
2021-05-27 23:05:16 +0530

    Update branchdbconnect.php

--- Branch/branchdbconnect.php
+++ Branch/branchdbconnect.php
@@ -1,5 +1,4 @@
 <?php 
 require_once __DIR__ . '/mongo/vendor/autoload.php';
-// $con = new MongoDB\Client("mongodb://localhost:27017");
-$con = new MongoDB\Client("mongodb+srv://mongouser:mongo@cluster0.gxm2b.mongodb.net/Product_data?retryWrites=true&w=majority");
+$con = new MongoDB\Client("mongodb://localhost:27017");
  ?>
